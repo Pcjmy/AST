fbae1f5aa4161ccdba9e17e26fda9b91094153f2
Pcjmy <1716045191@qq.com>
2024-08-17 23:15:30 +0800

    feat: 代码优化

--- bin/utils.js
+++ bin/utils.js
@@ -15,18 +15,40 @@ const getComponents = (code) => {
   const source = j(code);
 
   // 查找所有的React组件
-  const components = source.find(j.VariableDeclarator);
+  const components = [
+    ...source.find(j.VariableDeclarator).paths(),
+    ...source.find(j.FunctionDeclaration).paths(),
+    ...source.find(j.ExportDefaultDeclaration).paths(),
+  ];
+
+  const printedComponents = new Set();
 
   // 打印组件名，起始行和结束行
   components.forEach(path => {
-    if (
-      path.node.init &&
-      (j.ArrowFunctionExpression.check(path.node.init) ||
-        j.FunctionExpression.check(path.node.init))
-    ) {
-      console.log(`Component Name: ${path.node.id.name}`);
-      console.log(`Start Line: ${path.node.loc.start.line}`);
-      console.log(`End Line: ${path.node.loc.end.line}`);
+    let name = '';
+    let startLine = 0;
+    let endLine = 0;
+
+    if (j.VariableDeclarator.check(path.node) && 
+        (j.ArrowFunctionExpression.check(path.node.init) || j.FunctionExpression.check(path.node.init))) {
+      name = path.node.id.name;
+      startLine = path.node.init.loc && path.node.init.loc.start.line;
+      endLine = path.node.init.loc && path.node.init.loc.end.line;
+    } else if (j.ExportDefaultDeclaration.check(path.node) && 
+      (j.FunctionDeclaration.check(path.node.declaration) || j.ArrowFunctionExpression.check(path.node.declaration) || j.FunctionExpression.check(path.node.declaration))) {
+      name = path.node.declaration.id ? path.node.declaration.id.name : 'default';
+      if (name === 'default' && j.FunctionDeclaration.check(path.node.declaration)) {
+        name = path.node.declaration.id.name;
+      }
+      startLine = path.node.loc && path.node.loc.start.line;
+      endLine = path.node.loc && path.node.loc.end.line;
+    }
+
+    if (name && !printedComponents.has(name)) {
+      console.log(`Component Name: ${name}`);
+      console.log(`Start Line: ${startLine}`);
+      console.log(`End Line: ${endLine}`);
+      printedComponents.add(name);
     }
   });
 }
--- test/1.tsx
+++ test/1.tsx
@@ -3,3 +3,11 @@ import React from 'react';
 const App = () => {
   return <div>123</div>
 }
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+    </div>
+  );
+}
