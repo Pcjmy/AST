f767ab47fea85194409debd2c996d5b831146f5a
Pcjmy <1716045191@qq.com>
2024-08-18 00:36:27 +0800

    feat: 代码优化

--- test/1.tsx
+++ test/1.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const a = 1;
 
 const App = () => {
   return <div>123</div>
@@ -9,5 +11,19 @@ export default function MyApp() {
     <div>
       <h1>Welcome to my app</h1>
     </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      You pressed me {count} times
+    </button>
   );
 }
