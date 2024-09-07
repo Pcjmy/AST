effe28f43273fd2e05999b2c167a88a20a44774a
Pcjmy <1716045191@qq.com>
2024-09-07 22:52:48 +0800

    feat: parser

--- transforms/getNode.js
+++ transforms/getNode.js
@@ -1,7 +1,48 @@
 import j from 'jscodeshift';
+import parser from '@babel/parser';
+
+const options = {
+  sourceType: 'module',
+  allowImportExportEverywhere: true,
+  allowReturnOutsideFunction: true,
+  startLine: 1,
+  tokens: true,
+  plugins: [
+    'asyncGenerators',
+    'decoratorAutoAccessors',
+    'bigInt',
+    'classPrivateMethods',
+    'classPrivateProperties',
+    'classProperties',
+    'decorators-legacy',
+    'doExpressions',
+    'dynamicImport',
+    'exportDefaultFrom',
+    'exportExtensions',
+    'exportNamespaceFrom',
+    'functionBind',
+    'functionSent',
+    'importAttributes',
+    'importMeta',
+    'nullishCoalescingOperator',
+    'numericSeparator',
+    'objectRestSpread',
+    'optionalCatchBinding',
+    'optionalChaining',
+    ['pipelineOperator', { proposal: 'minimal' }],
+    'throwExpressions',
+    'typescript',
+    'jsx',
+  ],
+};
 
 export const transform = (code) => {
-  const ast = j(code);
+  const ast = j(code, {
+    parser: {
+      parse: (source) =>
+        parser.parse(source, options),
+    }
+  });
   const nodes = ast.find(j.Program).nodes();
   const arr = nodes[0].body;
   arr.forEach((node) => {
