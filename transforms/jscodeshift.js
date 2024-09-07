66c1730a96dcf9097d71874819fe37aa32f6b54f
Pcjmy <1716045191@qq.com>
2024-09-07 21:12:07 +0800

    feat: jscodeshift

--- transforms/jscodeshift.js
+++ transforms/jscodeshift.js
@@ -1,7 +1,15 @@
 import j from 'jscodeshift';
 
 export const transform = () => {
-  const sourceCode = `const a = 1;`;
-  const ast = j(sourceCode).getAST();
-  console.log(JSON.stringify(ast, null, 2));
+  const sourceCode = `
+import React from 'react';
+import { Button } from 'antd';
+`;
+
+  const ast = j(sourceCode)
+  ast
+    .find(j.ImportDeclaration, { source: { value: "antd" } })
+    .forEach((path) => {
+      console.log(path.node.source.value);
+    });
 }
