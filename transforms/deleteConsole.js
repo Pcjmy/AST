8c036264d143ae57ab31b497f22b0ef509fc5909
Pcjmy <1716045191@qq.com>
2024-09-07 23:57:30 +0800

    feat: deleteConsole

--- transforms/deleteConsole.js
+++ transforms/deleteConsole.js
@@ -4,6 +4,12 @@ export const transform = () => {
   const sourceCode = `
 const a = 1;
 console.log(a);
+try {
+  const s = 'abc';
+  console.log(s);
+} catch (e) {
+  console.log(e);
+}
 `;
 
   const ast = j(sourceCode)
@@ -13,6 +19,17 @@ console.log(a);
         object: { name: 'console' },
         property: { name: 'log' }
       }
+    })
+    .filter(path => {
+      // 检查是否在 catch 语句块中
+      let parent = path.parent;
+      while (parent) {
+        if (parent.value.type === 'CatchClause') {
+          return false;
+        }
+        parent = parent.parent;
+      }
+      return true;
     })
     .remove();
   console.log(ast.toSource());
